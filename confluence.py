77075647db8e5bfe9f3e7db1a56370e66005baf8
RaykKretzschmar <rkre75@gmail.com>
2020-03-02 15:19:47 +0100

    refactor

--- confluence.py
+++ confluence.py
@@ -1,7 +1,30 @@
-from atlassian import Confluence
+from datetime import datetime, timedelta
+import os
+import random
 
-confluence = Confluence(
-    url='https://confluence.convales.de',
-    username='RaKret',
-    password='')
+# Set your GitHub email
+email = "YOUR_EMAIL_HERE"
 
+# Set the start date to 3 years ago from today
+start_date = datetime.now() - timedelta(days=3*365)
+current_date = start_date
+
+commit_messages = ["updated", "removed unnecessary code", "bugfix", "refactor", "fix", "add feature"]
+
+while current_date < datetime.now():
+    # Create a fake commit for each day with a random commit message
+    date = current_date.strftime('%m/%d/%Y %H:%M:%S')
+    commit_message = random.choice(commit_messages)
+    with open('fake.txt', 'a') as file:
+        file.write(date + '\n')
+    os.system('git add .')
+    os.system(f'git commit --date="{date}" -m "{commit_message}"')
+    
+    # Push the commit to GitHub
+    os.system('git push origin main')
+    
+    # Increment the current date by one day
+    current_date += timedelta(days=1)
+
+# Set your Git config email back to your original email
+os.system(f'git config --global user.email "{email}"')
