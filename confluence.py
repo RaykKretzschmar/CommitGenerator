67c38419ae43dff4d87be0ed3cc29780913dc186
RaykKretzschmar <rkre75@gmail.com>
2023-03-02 14:41:44 +0100

    removed cloud

--- confluence.py
+++ confluence.py
@@ -3,6 +3,5 @@ from atlassian import Confluence
 confluence = Confluence(
     url='https://confluence.convales.de',
     username='RaKret',
-    password='ATATT3xFfGF0BWPZaxK4JNBI-Uv9kLaaU1ZBwCwiX14a8sc7p7CRiuxLd1DICFSMyqbOlrhAICOYwm6Hn-RBHiPW4TuISfauMIU8yX2mzaxY4kFRzA9WQQVmv1RzDYxLEFUO1xhTCwxbXEdslNjIEcSU2GArKck2wLN0jf5PPe_fm6pPoJHniNc=0EB32CE2'
-    cloud=True)
+    password='ATATT3xFfGF0BWPZaxK4JNBI-Uv9kLaaU1ZBwCwiX14a8sc7p7CRiuxLd1DICFSMyqbOlrhAICOYwm6Hn-RBHiPW4TuISfauMIU8yX2mzaxY4kFRzA9WQQVmv1RzDYxLEFUO1xhTCwxbXEdslNjIEcSU2GArKck2wLN0jf5PPe_fm6pPoJHniNc=0EB32CE2')
 
